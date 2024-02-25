19124d996924312c1dc8889a274a6db9346d1c61
Minal Ahmed <mimalahmed6219@gmail.com>
2024-02-25 11:19:29 +0600

    create blog and about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div className="flex justify-center items-center pt-20">
+      <h1 className="from-neutral-300 text-3xl font-bold">
+        Coming soon! About More...
+      </h1>
+    </div>
+  );
+};
+
+export default About;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,11 @@
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="flex justify-center items-center pt-10">
+      <h1 className="from-neutral-300 text-3xl font-bold">
+        Coming soon! All Blog
+      </h1>
+    </div>
+  );
 };
 
 export default Blog;
