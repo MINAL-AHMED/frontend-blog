92f22eeb88ca5db58ae1a8ba86073ae90a2cefb6
Minal Ahmed <mimalahmed6219@gmail.com>
2024-02-29 21:33:55 +0600

    some changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,10 @@
+import HeroVideo from "../components/HeroVideo";
+
 const About = () => {
   return (
     <div className="flex justify-center items-center pt-20">
       <h1 className="from-neutral-300 text-3xl font-bold">
-        Coming soon! About More...
+        <HeroVideo />
       </h1>
     </div>
   );
