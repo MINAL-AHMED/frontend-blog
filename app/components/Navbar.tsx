2bd9cd37e2ddaf0c200cec8ee478f167fcd6fcb3
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-01 20:40:38 +0600

    create a new navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
-    <header className="w-full border-b">
+    <header className="w-full fixed inset-x-0 top-0 z-30 mx-auto max-w-screen-md bg-white/30 shadow backdrop-blur-lg md:top-3 md:rounded-3xl lg:max-w-screen-lg dark:bg-white/40">
       <nav className="flex items-center justify-between max-w-3xl mx-auto px-4 py-5">
         <Link href="/" className="w-36 font-bold text-3xl">
           Tom<span className="text-primary">Blog</span>
