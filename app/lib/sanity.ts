00aaf81de504413a35d8b748efbd6c8cb8e4860a
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-05 20:49:27 +0600

    add video url

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -12,3 +12,16 @@ const builder = imageUrlBuilder(client);
 export function urlFor(source: any) {
   return builder.image(source);
 }
+
+// video url generation
+export function videoUrlFor(source: any) {
+  if (!source) return "";
+
+  if (source.asset) {
+    // If source is an asset reference
+    return `${source.asset.url}?autoplay=1`;
+  } else {
+    // If source is a direct URL
+    return source;
+  }
+}
